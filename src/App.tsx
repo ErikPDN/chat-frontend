f864028a1d6ea0a2beb18e7de9d2a9d972831ee5
Erik Neves <erikpdn1@gmail.com>
2026-01-11 20:36:08 -0300

    feat: começando a criação da LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { ToastContainer } from "./shared/components/ToastContainer"
 import RegisterPage from "./pages/RegisterPage"
 import { PublicRoute } from "./shared/components/ProtectedRoute"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route element={<PublicRoute />}>
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />}
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,38 @@
-export default function LoginPage() {
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../features/auth/stores/useAuthStore";
+import { useToast } from "../shared/hooks/useToast";
+import type { useState } from "react";
+import Input from "../shared/components/Input";
+import Button from "../shared/components/Button";
 
+export default function LoginPage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const setAuth = useAuthStore((state) => state.setAuth);
+  const { addToast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    rememberMe: false,
+  })
+
+  const hadleSubmit = async () => {
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  }
+
+  return (
+    <form action="">
+      <Input label="Email" name="email" onChange={handleChange} />
+      <Input />
+      <label>
+        <input type="checkbox" checked={formData.rememberMe} onChange={handleChange} /> Lembrar-me
+      </label>
+      <Button type="submit" disabled={isLoading} onClick={hadleSubmit} />
+      <a href="/forgot-password">Esqueci minha senha</a>
+    </form>
+  )
 }
